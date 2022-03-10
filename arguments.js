43b5026b9e48efc77b874bccb60be198e6ec7894
w0dm4n <w0dm4n@hotmail.fr>
2022-03-10 01:17:26 +0100

    last release sc csc

---
+++ arguments.js
@@ -0,0 +1,3 @@
+module.exports = [
+    "Celestials Stellar Club", "CSC", "https://ipfs.io/ipfs/QmQVYFbVoBja5eh2v5Ci9HBCF3FeNuXMEGP8Ku8KpwYVcp/", "https://ipfs.io/ipfs/QmXRGWHRnWWYVwAbwqK9vDsbUEW7ETZ8rDH46UozgehWKk/1.json"
+  ];
